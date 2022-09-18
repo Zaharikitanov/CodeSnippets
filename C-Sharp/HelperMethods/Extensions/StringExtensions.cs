43b5e3430d116e63aa3eb2b445daadd6e7205929
Zahari.Kitanov@stp-online.de <Zahari.Kitanov@stp-online.de>
2022-09-18 16:45:14 +0300

    C sharp coverage

---
+++ C-Sharp/HelperMethods/Extensions/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace HelperMethods.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ComputeSha1Hash(this string input)
+        {
+            // Create a SHA1   
+            using (SHA1 sha1Hash = SHA1.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha1Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
---
+++ C-Sharp/HelperMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelperMethods
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var test = new StringHelpers();
+
+            test.UrlContainsFile("https://cdn.skoda.at/media/Model_Stage_Image_Component/4756-stage-image/dh-991-e9d92a/767967fa/1656916962/skoda-octavia-rs-m67-introduction?id=2&name=Peter", out string result);
+
+            var outcome = result;
+        }
+    }
+}
+   
