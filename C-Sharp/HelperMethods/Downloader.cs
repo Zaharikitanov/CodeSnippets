79164ebb90c21718dde6ca4f9a2dde7e1b5b1572
Zahari.Kitanov@stp-online.de <Zahari.Kitanov@stp-online.de>
2023-02-07 08:56:05 +0200

    .

---
+++ C-Sharp/HelperMethods/Downloader.cs
@@ -0,0 +1,48 @@
+﻿namespace HelperMethods
+{
+    class Downloader
+    {
+        private static volatile int _totalBytes = 0;
+        private static DateTime _lastEpox = DateTime.Now;
+        public static int MaxThrottledBytes { get; set; }
+
+        public static void ReadFully(Stream input, string outputPath)
+        {
+            int bufferSize = 16 * 1024 > MaxThrottledBytes ? MaxThrottledBytes : 16 * 1024;
+            byte[] buffer = new byte[bufferSize];
+            using (FileStream localFileStream = new FileStream(outputPath, FileMode.OpenOrCreate))
+            {
+                int read;
+                for (; ; )
+                {
+                    // Throttle
+                    DateTime now = DateTime.Now;
+                    TimeSpan difference = now - _lastEpox;
+                    if (difference.TotalMilliseconds > 1000)
+                    {
+                        _lastEpox = now;
+                        _totalBytes = 0;
+                    }
+                    // Download
+                    if (_totalBytes + buffer.Length > MaxThrottledBytes)
+                    {
+                        Thread.Sleep(10);
+                    }  
+                    else
+                    {
+                        read = input.Read(buffer, 0, buffer.Length);
+                        if (read > 0)
+                        {
+                            localFileStream.Write(buffer, 0, read);
+                            _totalBytes += read;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- C-Sharp/HelperMethods/Program.cs
+++ C-Sharp/HelperMethods/Program.cs
@@ -3,90 +3,34 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
-namespace ParallelExample
+namespace HelperMethods
 {
     class Program
     {
         static async Task Main()
         {
-            var urls = new[] {
-                    "https://github.com/naudio/NAudio",
-                    "https://twitter.com/mark_heath",
-                    "https://github.com/markheath/azure-functions-links",
-                    //"https://pluralsight.com/authors/mark-heath",
-                    "https://github.com/markheath/advent-of-code-js",
-                    "http://stackoverflow.com/users/7532/mark-heath",
-                    "https://mvp.microsoft.com/en-us/mvp/Mark%20%20Heath-5002551",
-                    "https://github.com/markheath/func-todo-backend",
-                    "https://github.com/markheath/typescript-tetris",
-            };
-            var client = new HttpClient();
-            var maxThreads = 4;
-            var q = new ConcurrentQueue<string>(urls);
-            var tasks = new List<Task>();
-            for (int n = 0; n < maxThreads; n++)
-            {
-                tasks.Add(Task.Run(async () => {
-                    while (q.TryDequeue(out string url))
-                    {
-                        var html = await client.GetStringAsync(url);
-                        Console.WriteLine($"retrieved {html.Length} characters from {url}");
-                    }
-                }));
-            }
-            await Task.WhenAll(tasks);
+            await DownloadFile();
         }
 
-
-        /// <summary>
-        /// GetPrimeList returns Prime numbers by using sequential ForEach
-        /// </summary>
-        /// <param name="inputs"></param>
-        /// <returns></returns>
-        private static IList<int> GetPrimeList(IList<int> numbers) => numbers.Where(IsPrime).ToList();
-
-        /// <summary>
-        /// GetPrimeListWithParallel returns Prime numbers by using Parallel.ForEach
-        /// </summary>
-        /// <param name="numbers"></param>
-        /// <returns></returns>
-        private static IList<int> GetPrimeListWithParallel(IList<int> numbers)
+        private static async Task DownloadFile()
         {
-            var primeNumbers = new ConcurrentBag<int>();
+            var downloadUrl = "https://cdn.skoda.at/media/Model_Stage_Image_Component/4756-stage-image/dh-991-e9d92a/767967fa/1656916962/skoda-octavia-rs-m67-introduction.png";
+            var filename = "skoda.png";
+            
+            using var client = new HttpClient();
+            using var fileStream = new MemoryStream(await client.GetByteArrayAsync(downloadUrl));
+            Downloader.MaxThrottledBytes = 10000;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            Console.WriteLine("Started");
 
-            Parallel.ForEach(numbers, number =>
-            {
-                if (IsPrime(number))
-                {
-                    primeNumbers.Add(number);
-                }
-            });
+            Downloader.ReadFully(fileStream, $"new_{filename}");
 
-            return primeNumbers.ToList();
-        }
+            stopwatch.Stop();
+            Console.WriteLine($"Elapsed: {stopwatch.ElapsedMilliseconds}");
 
-        /// <summary>
-        /// IsPrime returns true if number is Prime, else false.(https://en.wikipedia.org/wiki/Prime_number)
-        /// </summary>
-        /// <param name="number"></param>
-        /// <returns></returns>
-        private static bool IsPrime(int number)
-        {
-            if (number < 2)
-            {
-                return false;
-            }
-
-            for (var divisor = 2; divisor <= Math.Sqrt(number); divisor++)
-            {
-                if (number % divisor == 0)
-                {
-                    return false;
-                }
-            }
-            return true;
         }
     }
 }
